3d233b923a2320defbde90fd1ab1e53f2afcadc8
Ludvig Sten <ludvig.stenman@elev.ga.ntig.se>
2019-08-29 10:52:43 +0200

    commit 2

--- test.php
+++ test.php
@@ -10,6 +10,15 @@
     <?php
         $name = "Ludvig";
         echo "Hello $name";
+
+        $all1 = 12; // integer
+        $all2 = (int) "123"; // string
+        $all3 = true; //boolean
+        $all4 = (int) 220,123; // double
+        $all5 = null;
+
+        echo $all1 + $all2 + $all3 + $all4 + $all5;
+
         ?>
 </body>
 </html>
